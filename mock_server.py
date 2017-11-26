bdfb1d8254381679e96bcadabd7e498d0a9c883d
shipYang <626999291@qq.com>
2017-11-26 22:56:41 +0800

    Update mock_server.py

--- mock_server.py
+++ mock_server.py
@@ -1,5 +1,4 @@
 from flask import Flask,request,make_response,jsonify
-import string
 
 
 app = Flask(__name__)
@@ -7,7 +6,7 @@ app = Flask(__name__)
 
 def read_mock():
     with open('/Users/YangYang/Desktop/mock_data.csv', 'r',encoding='utf-8') as f:
-        return (generate_mock_data(line) for line in f.readlines())
+        return [generate_mock_data(line) for line in f.readlines()]
 #从文件中读取数据，并且返回，把返回的值用表达式传给generate_mock_data
 
 
@@ -26,22 +25,40 @@ def checkpath(path,request_param,request_header,method):
         sub_request_param = [i]
         l.append(sub_request_param)
     if len(l) == 0:
-        for j in mock_data:
-            if path in j and method in j and request_header in j:
-                return generate_response(j[5], j[4],j[6])
+        if request_header!=None:
+            for j in range(len(mock_data)):
+                if path in mock_data[j] and method in mock_data[j] and request_header in mock_data[j] and mock_data[j][2]=='':
+                    return generate_response(mock_data[j][5], mock_data[j][4], mock_data[j][6])
+
+        else:
+            for j in range(len(mock_data)):
+                if path in mock_data[j] and method in mock_data[j] and mock_data[j][2]=='' and mock_data[j][3]=='':
+                    return generate_response(mock_data[j][5], mock_data[j][4], mock_data[j][6])
+
+
     else:
         format_reqparam = formate_params(l)  # 获取实际的请求参数,并进行格式化
-
-        for j in mock_data:
-            if path in j and (method in j) and (format_reqparam in j) and request_header in j:
-
-                return generate_response(j[5],j[4],j[6])
+        if request_header != None:
+            for j in range(len(mock_data)):
+                if path in mock_data[j] and (method in mock_data[j]) and (
+                    format_reqparam in mock_data[j]) and request_header in mock_data[j]:
+                    return generate_response(mock_data[j][5], mock_data[j][4], mock_data[j][6])
+        else:
+            for j in range(len(mock_data)):
+                if path in mock_data[j] and (method in mock_data[j]) and (
+                    format_reqparam in mock_data[j]) and mock_data[j][3]=='':
+                    return generate_response(mock_data[j][5], mock_data[j][4], mock_data[j][6])
+    
 
 #处理mock数据中预期的返回值
 def generate_response(resp_body,cookie,http_status):
-    resp=make_response(resp_body)
-    resp.set_cookie('cookie',cookie)
+
     if http_status=='200':
+       resp = make_response(resp_body)
+       if cookie =='None':
+           pass
+       else:
+          resp.set_cookie('cookie', cookie)
        return resp
     else:
         return make_response(jsonify({'msg':'error'}),http_status)
@@ -81,3 +98,5 @@ def get_task(path):
 
 if __name__ == '__main__':
     app.run(debug=True,port=5000,threaded=True)
+
+# print(mock_data)
